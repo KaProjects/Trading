4a411301ecb4c97ef0c7ec37d723c8ebf74cb0d5
Kaleta, Stanislav <stanislav.kaleta@oracle.com>
2025-05-10 19:40:32 +0200

    [processor] refactoring analyzer + bumped to 1.2

---
+++ processor/about_version.md
@@ -0,0 +1,18 @@
+## release workflow:
+     bump build_deploy.sh versions
+
+## 1.2
+Released: 10.5.2025
+
+    cmc consumer and discord supplier for fear and greed indicator
+    refactoring
+
+## 1.1
+Released: 27.4.2021
+
+    bug fixes and improvements
+
+## 1.0
+Released: 11.3.2021
+
+    alert consumer and resolver 
--- processor/analyzer.py
+++ processor/analyzer.py
@@ -1,12 +1,16 @@
-
 from firebase_admin import db
+
+import utils
+
+data_path = "log/"
 
 
 def analyze():
-    logs:dict = db.reference("log/").get()
+    logs: dict = db.reference(data_path).get()
     logs_by_tag = sort_by_tag(logs.values())
-    for log in logs_by_tag["AAL"]:
-        print("{} | {}".format(log["time"],log["type"]))
+    for log in logs_by_tag["NVDA"]:
+        print("{} | {}".format(log["time"], log["type"]))
+
 
 def sort_by_tag(logs):
     logs_by_tag = dict()
@@ -15,3 +19,24 @@ def sort_by_tag(logs):
             logs_by_tag[log["ticker"]] = list()
         logs_by_tag[log["ticker"]].append(log)
     return logs_by_tag
+
+
+def prune_db():
+    counter = 1
+    while True:
+        print("Deleting {}. mile".format(counter))
+        logs = db.reference(data_path).order_by_key().limit_to_last(1000).get()
+        for key in logs:
+            db.reference(data_path + "/" + key).delete()
+        if len(logs) < 1000: break;
+        counter = counter + 1
+
+
+if __name__ == '__main__':
+    envs = utils.parse("envs.json")
+    utils.init_firebase(envs["firebase"])
+    # db.reference(data_path).delete()
+    # prune_db()
+    analyze()
+
+
