4e005ca77b3ee9e090ee85b2944f7ee3e53eda97
Kaleta, Stanislav <stanislav.kaleta@oracle.com>
2025-10-18 19:18:42 +0200

    [processor] introduced scheduler + fng rewritten as cron job

--- processor/fear_and_greed_discord.py
+++ processor/fear_and_greed_discord.py
@@ -1,4 +1,3 @@
-import asyncio
 import json
 import traceback
 
@@ -12,67 +11,65 @@ def log(message: str):
     utils.log("FNG", message)
 
 
-def request(path: str, parameters: object, api_key):
-    url = "https://pro-api.coinmarketcap.com" + path
-    headers = {
-        "Accepts": "application/json",
-        "X-CMC_PRO_API_KEY": api_key,
-    }
+class BtcFngDiscordRunner:
+    def __init__(self, discord_api_key, cmc_api_key):
+        self.discord_api_key = discord_api_key
+        self.cmc_api_key = cmc_api_key
+        self.last_value = -1
 
-    session = Session()
-    session.headers.update(headers)
-
-    try:
-        response = session.get(url, params=parameters)
-        if response.status_code != 200:
-            log(response.text)
-        else:
-            return json.loads(response.text)
-    except (ConnectionError, Timeout, TooManyRedirects) as e:
-        log(e)
-
-
-def post(payload: object, api_key: str):
-    url = "https://discord.com/api/webhooks/" + api_key
-    headers = {"Content-Type": "application/json"}
-
-    session = Session()
-    session.headers.update(headers)
-
-    try:
-        response = session.post(url, data=json.dumps(payload))
-        if response.status_code != 204:
-            log(response.text)
-    except (ConnectionError, Timeout, TooManyRedirects) as e:
-        log(e)
-
-
-async def run(envs):
-    discord_api_key = envs["discord_api_key"]
-    cmc_api_key = envs["cmc_api_key"]
-    last_value = -1
-    while True:
+    def run(self):
         try:
-            data = request("/v3/fear-and-greed/latest", {}, cmc_api_key)
+            data = self.cmc_request("/v3/fear-and-greed/latest", {})
             if data and "data" in data:
                 classification = data["data"]["value_classification"]
                 new_value = int(data["data"]["value"])
-                if last_value//10 != new_value//10:
-                    btc_data = request("/v2/cryptocurrency/quotes/latest", {'symbol': 'BTC'}, cmc_api_key)
-                    if btc_data and "data" in btc_data:
-                        btc_price = btc_data["data"]["BTC"][0]["quote"]["USD"]["price"]
-                        message = "BTC=${:.0f} & {}: {} -> {}".format(btc_price, classification, last_value, new_value)
-                        log(message)
-                        if last_value != -1: post({"content": message}, discord_api_key)
-                    else:
-                        log("Invalid BTC data: {}".format(btc_data))
+
+                btc_data = self.cmc_request("/v2/cryptocurrency/quotes/latest", {'symbol': 'BTC'})
+                if btc_data and "data" in btc_data:
+                    btc_price = btc_data["data"]["BTC"][0]["quote"]["USD"]["price"]
+                    message = "BTC=${:.0f} & {}: {} -> {}".format(btc_price, classification, self.last_value, new_value)
+                    log(message)
+                    if new_value not in range(40, 60):
+                        self.discord_post({"content": message})
                 else:
-                    log("New FnG value: {}".format(new_value))
-                last_value = new_value
+                    log("Error: Invalid BTC data: {}".format(btc_data))
+
+                self.last_value = new_value
             else:
                 log(data)
         except Exception:
             log(traceback.format_exc())
             log("^^^ exception occurred!")
 
-        await asyncio.sleep(60 * 60)
+    def cmc_request(self, path: str, parameters: object):
+        url = "https://pro-api.coinmarketcap.com" + path
+        headers = {
+            "Accepts": "application/json",
+            "X-CMC_PRO_API_KEY": self.cmc_api_key,
+        }
+
+        session = Session()
+        session.headers.update(headers)
+
+        try:
+            response = session.get(url, params=parameters)
+            if response.status_code != 200:
+                log(response.text)
+            else:
+                return json.loads(response.text)
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            log(e)
+
+    def discord_post(self, payload: object):
+        url = "https://discord.com/api/webhooks/" + self.discord_api_key
+        headers = {"Content-Type": "application/json"}
+
+        session = Session()
+        session.headers.update(headers)
+
+        try:
+            response = session.post(url, data=json.dumps(payload))
+            if response.status_code != 204:
+                log(response.text)
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            log(e)
--- processor/main.py
+++ processor/main.py
@@ -1,16 +1,25 @@
 import asyncio
-
-import fear_and_greed_discord
+import schedule
 import tradingview_alert_processor
 import utils
 
+from fear_and_greed_discord import BtcFngDiscordRunner
 
 async def gather():
     await asyncio.gather(
-        fear_and_greed_discord.run(envs),
+        cron(),
         tradingview_alert_processor.run()
     )
 
+async def cron():
+    btc_fng_discord_runner = BtcFngDiscordRunner(envs["discord_api_key"], envs["cmc_api_key"])
+
+    # schedule.every(5).seconds.do(btc_fng_discord_runner.run)
+    schedule.every().day.at("05:00").do(btc_fng_discord_runner.run)
+
+    while True:
+        schedule.run_pending()
+        await asyncio.sleep(1)
 
 if __name__ == '__main__':
     envs = utils.parse("envs.json")
