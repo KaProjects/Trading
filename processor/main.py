8872a80f3f9d52dbaceafa615a6ee18b8bada764
Kaleta, Stanislav <stanislav.kaleta@oracle.com>
2025-10-19 15:05:32 +0200

    [processor] fng time change

--- processor/main.py
+++ processor/main.py
@@ -15,7 +15,7 @@ async def cron():
     btc_fng_discord_runner = BtcFngDiscordRunner(envs["discord_api_key"], envs["cmc_api_key"])
 
     # schedule.every(5).seconds.do(btc_fng_discord_runner.run)
-    schedule.every().day.at("05:00").do(btc_fng_discord_runner.run)
+    schedule.every().day.at("03:00").do(btc_fng_discord_runner.run)
 
     while True:
         schedule.run_pending()
