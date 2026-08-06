e43ec5e9fc8dd09a6f5239dd8d5ee3df1d7b0a43
Kaleta, Stanislav <stanislav.kaleta@oracle.com>
2026-08-06 13:17:13 +0200

    fix migrations

--- backend/sql/migrate-1.3-2.0.sql
+++ backend/sql/migrate-1.3-2.0.sql
@@ -1,5 +1,29 @@
-CREATE TABLE Period(...);
-CREATE TABLE Latest(...);
+CREATE TABLE Period
+(
+    id           VARCHAR(36)   NOT NULL PRIMARY KEY,
+    name         CHAR(4)       NOT NULL,
+    ending_month CHAR(4)       NOT NULL,
+    report_date  DATE,
+    research     TEXT,
+    price_low    DECIMAL(10, 4),
+    price_high   DECIMAL(10, 4),
+    shares       DECIMAL(8, 2),
+    revenue      DECIMAL(8, 2),
+    gross_profit DECIMAL(8, 2),
+    oper_income  DECIMAL(8, 2),
+    net_income   DECIMAL(8, 2),
+    dividend     DECIMAL(8, 2),
+    companyId    VARCHAR(36)   NOT NULL,
+    CONSTRAINT `fk_periodCompanyId` FOREIGN KEY (companyId) REFERENCES Company (id)
+);
+CREATE TABLE Latest
+(
+    id           VARCHAR(36)     NOT NULL PRIMARY KEY,
+    datetime     DATETIME        NOT NULL,
+    price        DECIMAL(10, 4)  NOT NULL,
+    companyId    VARCHAR(36)     NOT NULL,
+    CONSTRAINT `fk_latestCompanyId` FOREIGN KEY (companyId) REFERENCES Company (id)
+);
 
 DROP TABLE Financial;
 
@@ -15,9 +39,4 @@ ALTER TABLE Record ADD COLUMN p_gross DECIMAL(6, 2);
 ALTER TABLE Record ADD COLUMN p_oper DECIMAL(6, 2);
 ALTER TABLE Record ADD COLUMN asset_quantity DECIMAL(8, 4);
 ALTER TABLE Record ADD COLUMN asset_price DECIMAL(10, 4);
-
-ALTER TABLE Period ADD COLUMN gross_profit DECIMAL(8, 2);
-ALTER TABLE Period ADD COLUMN oper_income DECIMAL(8, 2);
-ALTER TABLE Period DROP COLUMN cogs;
-ALTER TABLE Period DROP COLUMN op_exp;
 
--- backend/sql/migrate-2.0-2.1.sql
+++ backend/sql/migrate-2.0-2.1.sql
@@ -4,10 +4,28 @@ ALTER TABLE Record MODIFY COLUMN strategy TEXT;
 ALTER TABLE Record ADD COLUMN review TEXT;
 ALTER TABLE Record ADD COLUMN retro TEXT;
 
-CREATE TABLE Estimate(...);
+CREATE TABLE Estimate
+(
+    id           INT UNSIGNED    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    datetime     DATETIME        NOT NULL,
+    current      DECIMAL(6, 2)   NOT NULL,
+    next1        DECIMAL(6, 2),
+    next2        DECIMAL(6, 2),
+    next3        DECIMAL(6, 2),
+    periodId     INT UNSIGNED    NOT NULL,
+    CONSTRAINT `fk_estimatePeriodId` FOREIGN KEY (periodId) REFERENCES Period (id)
+);
+
+ALTER TABLE Period ADD COLUMN adjusted_eps DECIMAL(6, 2);
 
 ALTER TABLE Trade ADD COLUMN portfolio VARCHAR(30);
 
-CREATE TABLE Tag(...);
+CREATE TABLE Tag
+(
+    id           INT UNSIGNED    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    value        VARCHAR(30)     NOT NULL,
+    companyId    INT UNSIGNED    NOT NULL,
+    CONSTRAINT `fk_tagCompanyId` FOREIGN KEY (companyId) REFERENCES Company (id)
+);
 
 ALTER TABLE Company DROP COLUMN watching;
